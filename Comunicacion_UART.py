6ab5433112aa89080d8f6b74a01b801397feeefb
Jorge Brenes <jorgebre2098@hotmail.com>
2022-08-04 08:51:26 -0600

    Correcciones al código

--- Comunicacion_UART.py
+++ Comunicacion_UART.py
@@ -8,21 +8,36 @@
 #NVIDIA Jetson TX 2 con un PSoC con el fin de enviar datos para controlar
 #una planta de péndulo amortiguado a hélice (PAHM).
 
-import Jetson.GPIO
+#import Jetson.GPIO
 import serial
 import time
 
-pin_transmit = 8
-pint_receive = 10
+#pin_transmit = 8
+#pint_receive = 10
 #pwm_value = randint(0,4)
 
-#pin_transmit = serial.Serial('??', baudrate=115200, parity=serial.PARITY.NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
+serial_port = serial.Serial('dev/ttyTHS1', baudrate=115200, parity=serial.PARITY.NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 #pin_receive = serial.Serial('??', baudrate=115200, parity=serial.PARITY.NONE, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS, timeout=1)
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
-GPIO.setup(pin_transmit, GPIO.OUT, initial=GPIO.HIGH)  # pin_transmit set as output
-GPIO.setup(pin_receive, GPIO.IN, initial=GPIO.HIGH)   # pin_transmit set as output
+time.sleep(1)
+
+serial_port.write('Demostración de UART')
+while True:
+    if serial_port.inWaiting() > 0:
+        datos = serial_port.read()
+        print('Se recibe: ', datos)
+
+        serial_port.write(datos)
+        if datos == '/r'.encode():
+            serial_port.write('/n'.encode())
+            
+serial_port.close()
+
+
+#GPIO.setmode(GPIO.BOARD) GPIO.setwarnings(False)
+#GPIO.setup(pin_transmit, GPIO.OUT, initial=GPIO.HIGH)  # pin_transmit
+#set as output GPIO.setup(pin_receive, GPIO.IN, initial=GPIO.HIGH)  #
+#pin_transmit set as output
 
 #while True:
     #pwm_value = randint(0,4)
@@ -31,19 +46,19 @@ GPIO.setup(pin_receive, GPIO.IN, initial=GPIO.HIGH)   # pin_transmit set as outp
     #pwm_value = RL_controller(rx_data) # futuramente se llama al controlador de RL
     #pin_transmit.write(pwm_value)
 
-try:
-    while True:
-        pwm_value = randint(0,4)
-        print('PWM value is: ', pwm_value)
-        print('***** Data Receiving from PSoC *****')
-        rx_data = GPIO.input(pin_receive)
-        print('Data value is: ', rx_data)
-        time.sleep(1)
-        print('***** Data Transmitting from Jetson *****')
-        GPIO.output(pwm_value)
-        time.sleep(1)
-finally:
-    GPIO.cleanup()
+#try:
+#    while True:
+        #pwm_value = randint(0,4)
+        #print('PWM value is: ', pwm_value)
+        #print('***** Data Receiving from PSoC *****')
+        #rx_data = GPIO.input(pin_receive)
+        #print('Data value is: ', rx_data)
+        #time.sleep(1)
+        #print('***** Data Transmitting from Jetson *****')
+        #GPIO.output(pwm_value)
+        #time.sleep(1)
+#finally:
+    #GPIO.cleanup()
 
 
 # $ /boot/config.txt
