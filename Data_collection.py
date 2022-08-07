eec73353968355f1a1c2feecebda00ee00384590
Jorge Brenes <jorgebre2098@hotmail.com>
2022-08-07 17:05:37 -0600

    Se agrega función para hacer archivo .xlsx

--- Data_collection.py
+++ Data_collection.py
@@ -9,33 +9,47 @@
 import time
 import serial
 import random
+import xlsxwriter
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
 
-
-# Definici[on del puerto
+# Definicion del puerto
 serial_port = serial.Serial("/dev/ttyTHS2", 
 			baudrate=115200,
 			stopbits=serial.STOPBITS_ONE,
 			bytesize=serial.EIGHTBITS)
 
-time.sleep(0.002)
+time.sleep(0.02)
 
 serial_port.write('Demostracion de UART'.encode())
 serial_port.write('\r\nNVIDIA Jetson TX2\r\n'.encode())
 
+values = [[0,0,0]]
 cont = 0
-initial_value = int(input("Valor inicial del PWM (0-4): "))
+entrada  = str(input("Elija una entrada: "))
 
-while cont < 20:
+while cont <= len(entrada):
     if serial_port.inWaiting() > 0:
-        serial_port.write(initial_value)
-        datos = serial_port.read()
-        print("Datos recibidos: ", datos)
-        pwm_value = round(random.uniform(0,4))
+
+        if entrada == 'Escalon':
+            pwm_value = step[cont] # Entrada escalón
+        elif entrada == 'Rampa':
+            pwm_value= r[cont] # Entrada rampa
+        elif entrada == 'Random':
+            pwm_value= round(random.uniform(0,4),10) # Entrada random
+        else:
+            print("Entrada inválida.")
+            pwm_value = 0
+        
+        ini = time.time()
         serial_port.write(pwm_value)
-        print("Datos enviados: ", pwm_value)
+        angle = serial_port.read()
+        fin = time.time()
+        latencia = fin-ini
+        values = [latencia, pwm_value,angle]
+        print("\r\nLatencia: "latencia,"\r\nDatos recibidos: ", datos, "\r\nDatos tansmitidos: ", pwm_value)        
         cont += 1 
+archivo_excel(values)
 serial_port.close()
 
 
@@ -43,12 +57,30 @@ star, end, sampling = -2,10,0.02
 time = np.concatenate([np.arange(star,end,sampling), np.zeros(1000)])
 Amplitude = round(random.uniform(0,4),4)
 
+
+# ********************************** Función Escalón **********************************#
 # Función escalón
 def step_function(A,t):
     u = (t >= 0)*A
     return u
 
+# ********************************** Función Rampa**********************************#
 # Función rampa
 def ramp_function(A,t):
     r = t*step_function(A,t)
     return r
+
+# ********************************** Función para generar archivo .xlsx **********************************#
+def archivo_excel(values):
+    archivo = xlsxwriter.Workbook("Data_collection.xlsx")
+    hoja = archivo.add_worksheet()
+    names = ['Latencia','Entrada','Ángulo']
+    for j in range(len(names)):
+        hoja.write(0,j,names[j])
+
+    for j in range(len(values)):
+        for i in range(len(values[j])):
+            hoja.write(j+1,i,values[j][i]) # row, col, data
+    archivo.close()
+
+# **********************************  **********************************#
