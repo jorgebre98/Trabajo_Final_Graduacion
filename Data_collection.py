60e794ae3af52ddb551aa64994fbd315b3321e09
JorgeBrenes <jorgebre2098@hotmail.com>
2022-08-08 16:46:02 -0600

    Se deben realizar correcciones

--- Data_collection.py
+++ Data_collection.py
@@ -27,19 +27,21 @@ serial_port.write('\r\nNVIDIA Jetson TX2\r\n'.encode())
 values = [[0,0,0]]
 cont = 0
 
-while cont <= 1000:
-        if serial_port.inWaiting() > 0:
-                pwm_value= round(random.uniform(0,4),10) # Entrada random
+while True:
+#while cont <= 1000:
+	#if serial_port.inWaiting() > 0:
+        pwm_value= str(round(random.uniform(0,4),10)).encode() # Entrada random
                 #pwm_value = step[cont] # Entrada escalón
                 #pwm_value= r[cont] # Entrada rampa
-                ini = time.time()
-                serial_port.write(pwm_value)
-                angle = serial_port.read()
-                fin = time.time()
-                latencia = fin-ini
-                values = [latencia, pwm_value,angle]
-                print("\r\nLatencia: ", latencia,"\r\nDatos recibidos: ", datos, "\r\nDatos tansmitidos: ", pwm_value)        
-                cont += 1
+        ini = time.time()
+        serial_port.write(pwm_value)
+        angle = serial_port.read()
+        fin = time.time()
+        latencia = fin-ini
+        values = [latencia, pwm_value,angle]
+        print('Datos recibidos: ',angle)
+#print("\r\nLatencia: ", latencia,"\r\nDatos recibidos: ", datos, "\r\nDatos tansmitidos: ", pwm_value)        
+        #        cont += 1
 archivo_excel(values)
 serial_port.close()
 
