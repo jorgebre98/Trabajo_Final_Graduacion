513522ffcd72e3e777cd1c90e45d353d57e0cdad
JorgeBrenes <jorgebre2098@hotmail.com>
2022-08-09 14:51:58 -0600

    Se terminan pruebas del código

--- Data_collection.py
+++ Data_collection.py
@@ -11,25 +11,22 @@ import serial
 import random
 import xlsxwriter
 import numpy as np
-import pandas as pd
 
 
-# ********************************** Función Escalón **********************************#
-# Función escalón
+# ********************************** Step Function **********************************#
 def step_function(A,t):
     u = (t >= 0)*A
     return u
 
-# ********************************** Función Rampa**********************************#
-# Función rampa
+# ********************************** Ramp Function **********************************#
 def ramp_function(A,t):
     r = t*step_function(A,t)
     return r
 
-# ********************************** Función para generar archivo .xlsx **********************************#
+# *********************** Function to create .xlsx archives *************************#
 def archivo_excel(values):
-    archivo = xlsxwriter.Workbook("Data_collection.xlsx")
-    hoja = archivo.add_worksheet()
+    archive = xlsxwriter.Workbook("Data_collection.xlsx")
+    hoja = archive.add_worksheet()
     names = ['Latencia','Entrada','Ángulo']
     for j in range(len(names)):
         hoja.write(0,j,names[j])
@@ -37,9 +34,10 @@ def archivo_excel(values):
     for j in range(len(values)):
         for i in range(len(values[j])):
             hoja.write(j+1,i,values[j][i]) # row, col, data
-    archivo.close()
+    archive.close()
 
-# **********************************  **********************************#
+
+# ************************* Data Transmission and Reception *************************#
 
 # Definicion del puerto
 serial_port = serial.Serial("/dev/ttyTHS2", 
@@ -52,14 +50,22 @@ time.sleep(0.02)
 serial_port.write('Demostracion de UART'.encode())
 serial_port.write('\r\nNVIDIA Jetson TX2\r\n'.encode())
 
+
+star, end, sampling = -2,10,0.02
+tiempo = np.concatenate([np.arange(star,end,sampling), np.zeros(1000)])
+Amplitude = round(random.uniform(0,4),4)
+step = step_function(Amplitude,tiempo)
+r = ramp_function(Amplitude,tiempo)
+print('\r\nAmplitud: ',Amplitude,'\r\n')
+
 values = [[0,0,0]]
 cont = 0
 
 #while True:
-while cont <= 10:
+while cont <= 500:
         if serial_port.inWaiting() > 0:
-                pwm_value= str(round(random.uniform(0,4),10)).encode() # Entrada random
-                #pwm_value = step[cont] # Entrada escalón
+                #pwm_value= str(round(random.uniform(0,4),10)).encode() # Entrada random
+                pwm_value = step[cont] # Entrada escalón
                 #pwm_value= r[cont] # Entrada rampa
                 ini = time.time()
                 serial_port.write(str(pwm_value).encode())
@@ -67,7 +73,7 @@ while cont <= 10:
                 fin = time.time()
                 latencia = fin-ini
                 values.append([latencia, pwm_value,angle])
-                print('Datos recibidos: ',float(angle))
+                #print('Datos recibidos: ',float(angle))
                 print("\r\nLatencia: ", latencia,"\r\nDatos recibidos: ", angle, "\r\nDatos tansmitidos: ", pwm_value)        
                 cont += 1
 archivo_excel(values)
@@ -75,8 +81,6 @@ print('\r\nValores: ', values)
 serial_port.close()
 
 
-#star, end, sampling = -2,10,0.02
-#time = np.concatenate([np.arange(star,end,sampling), np.zeros(1000)])
-#Amplitude = round(random.uniform(0,4),4)
+
 
 
