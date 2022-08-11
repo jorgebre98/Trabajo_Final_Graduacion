ec9a37d7894e411ef555e00e4c9e81449437d96d
Jorge Brenes <jorgebre2098@hotmail.com>
2022-08-11 10:30:26 -0600

    Se crean varias funciones

--- programa.py
+++ programa.py
@@ -1,22 +1,61 @@
 import serial
 import struct
+import random
+import time
 
-ser = serial.Serial("/dev/ttyTHS2", 
-			baudrate=115200,
-			stopbits=serial.STOPBITS_ONE,
-			bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)
 
-value = 3.2
-bina = struct.pack('!f',value)  # The "!" ensures that it's in network byte order (big endian)
-print('Binario: ',bina)
+def Transmit_and_Receive(ser, num): # Serial port and float number
+        # float packed into bytes. The '!' ensures that it's in network byte order (big-endian).
+        packed = struct.pack('!f', num)
+        #print ('Characters: %s' % repr(packed), flush = True)
+        integers = [c for c in packed] # Each character correspond a interger.
+        #print ('Integers: %s' % integers, flush = True)
+        binaries = [bin(i) for i in integers] # Convert to binary representation.
+        stripped_binaries = [s.replace('0b', '') for s in binaries] # Strip off the '0b'.
+        padded = [s.rjust(8, '0') for s in stripped_binaries] # Make sure all has 8 bits.
+        binario = ".join(padded)
+        print('\r\nCharacters:', packed, '\r\nIntergers: ', intergers,'\r\nBinary to transmit: ', binario, flush = True)
 
-for b in bina:
-	print('Transmitting: ',b)
-	ser.write(b)
+        print("\r\n**Transmitting**\r\n")
+        start = time.time()
+        for b in packed:
+                ser.write(b)
+        print('**Transmission Finished**')
+        
+        print('**Receiving**')
+        data_receive = ser.read(4)
+        latencia = time.time() - start
+        data = data.unpack('!f', data) # Convert bytes to float
+        print('Data Receive: ', data)
+        return data_receive, latencia
 
-print('**Transmission Finished**')
-print('**Receiving**')
-data = ser.read(4)
-data = struct.unpack('!f', data)
-print(data)
+def archivo_excel(values):
+    archive = xlsxwriter.Workbook("Data_collection.xlsx")
+    hoja = archive.add_worksheet()
+    names = ['Latencia','Entrada','Ángulo']
+    for j in range(len(names)):
+        hoja.write(0,j,names[j])
+
+    for j in range(len(values)):
+        for i in range(len(values[j])):
+            hoja.write(j+1,i,values[j][i]) # row, col, data
+    archive.close()
+
+
+# Port definition
+serial_port = serial.Serial("/dev/ttyTHS2",
+                    baudrate=115200,
+                    stopbits=serial.STOPBITS_ONE,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE)
+
+serial_port.open()
+cont = 0
+
+while cont <= 20:
+        pwm_value = 3.2
+        #value = random.uniform(0,4)
+        data_receive, latencia = Transmit_and_Receive (serial_port, pwm_value)
+        values = [[latencia, pwm_value, data_receive]]
+        archivo_excel(values)
 ser.close()
