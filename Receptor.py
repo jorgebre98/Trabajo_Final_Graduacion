6b8675ed4a00d58198d39bee11d1ef938831f716
Jorge Brenes <jorgebre2098@hotmail.com>
2022-08-16 17:28:18 -0600

    Se hace llamado a la función timer

--- Receptor.py
+++ Receptor.py
@@ -1,37 +1,8 @@
 import serial
 import struct
 import random
-from Timer import RepeatedTimer
-import threading 
-import time
-
-class RepeatedTimer(object):
-    def __init__(self, interval, function, *args, **kwargs):
-        self._timer = None
-        self.interval = interval
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-        self.is_running = False
-        self.next_call = time.time()
-        self.start()
-
-    def _run(self):
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
-
-    def start(self):
-        if not self.is_running:
-            self.next_call += self.interval
-            self._timer = threading.Timer(self.next_call - time.time(), self._run)
-            self._timer.start()
-            self.is_running = True
-
-    def stop(self):
-        self._timer.cancel()
-        self.is_running = False 
-    
+from Timer import *
+   
 def Receive(port,num):
     packed = struct.pack('f',num)
     data = port.read(size=4)
