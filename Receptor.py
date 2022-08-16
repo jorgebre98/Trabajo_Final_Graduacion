9a1b1a4c2dd79ae73798f5bbd242539546452a24
Jorge Brenes <jorgebre2098@hotmail.com>
2022-08-16 16:59:17 -0600

    Se agrega timer al receptor

--- Receptor.py
+++ Receptor.py
@@ -1,32 +1,60 @@
 import serial
 import struct
 import random
+from Timer import RepeatedTimer
+import threading 
+import time
 
-def binary(num):
-    print('Transmitting: ',num)
-    print('')
-    packed = struct.pack('!f', num)
-    print ('Characters: %s' % repr(packed))
-    integers = [c for c in packed] # Se obtiene los enteros de los caracteres.
-    binaries = [bin(i) for i in integers] # Se convierte cada entero a binario.
-    stripped_binaries = [s.replace('0b', '') for s in binaries] # Se quita '0b' de cada binario.
-    padded = [s.rjust(8, '0') for s in stripped_binaries] # Se ajusta el binario a 8 bits
-    val = ''.join(padded)
-    print('Binary to transmit: ',val)
-    return val,packed
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.next_call = time.time()
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self.next_call += self.interval
+            self._timer = threading.Timer(self.next_call - time.time(), self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False 
     
-   
-#serial_port = serial.Serial("/dev/ttyTHS2",baudrate=115200,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)
-serial_port = serial.Serial("/dev/ttyUSB0",baudrate=115200,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)
+def Receive(port,num):
+    packed = struct.pack('f',num)
+    data = port.read(size=4)
+    port.write(packed)
+    data_r = struct.unpack('f',data)
+    print('\nData receive: ',data_r[0],'\nData Transmit: ',num,flush=True)
+    
+def Data_collect(ser):
+    pwm_value = round(random.uniform(0,4),4)
+    data_receive = Receive(ser, pwm_value)
 
+#Definicion puerto serial
+serial_port = serial.Serial("/dev/ttyUSB0",
+                            baudrate=115200,
+                            stopbits=serial.STOPBITS_ONE,
+                            bytesize=serial.EIGHTBITS,
+                            parity=serial.PARITY_NONE)
 
-cont = 0
-while cont != 2:
-	data = serial_port.read(size=4)
-	data_2 = struct.unpack('!f', data)
-	print('Data receive: ', data)
-	print('Float Data Receive: ',data_2)
-	val, data_t = binary(round(random.uniform(0,4),4))
-	serial_port.write(data_t)
-	cont += 1
-serial_port.close()
+print('**********  Strarting********** ',flush=True)
+Data_collect(serial_port)
+rt = RepeatedTimer(0.02,Data_collect,serial_port)
+try:
+    time.sleep(10) # long running job
+finally:
+    rt.stop()
+    
