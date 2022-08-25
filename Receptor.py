912170afb6b0b83cf0ed68b903e597bcc310d927
JorgeBrenes <jorgebre2098@hotmail.com>
2022-08-24 18:06:08 -0600

    Se modifica el receptor

--- Receptor.py
+++ Receptor.py
@@ -4,28 +4,24 @@ import random
 from Timer import *
    
 def Receive(port,num):
-    packed = struct.pack('f',num)
+    #packed = struct.pack('f',num)
     data = port.read(size=4)
-    port.write(packed)
-    data_r = struct.unpack('f',data)
-    print('\nData receive: ',data_r[0],'\nData Transmit: ',num,flush=True)
+    #port.write(packed)
+    data_r = struct.unpack('i',data)
+    print('\nData receive: ',data_r[0])#,'\nData Transmit: ',num,flush=True)
     
 def Data_collect(ser):
     pwm_value = round(random.uniform(0,4),4)
     data_receive = Receive(ser, pwm_value)
 
 #Definicion puerto serial
-serial_port = serial.Serial("/dev/ttyUSB0",
-                            baudrate=115200,
-                            stopbits=serial.STOPBITS_ONE,
-                            bytesize=serial.EIGHTBITS,
-                            parity=serial.PARITY_NONE)
+serial_port = serial.Serial("/dev/ttyTHS2",baudrate=115200,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,parity=serial.PARITY_NONE)
 
-print('**********  Strarting********** ',flush=True)
+print('**********  Starting ********** ',flush=True)
 Data_collect(serial_port)
 rt = RepeatedTimer(0.02,Data_collect,serial_port)
 try:
-    time.sleep(10) # long running job
+    time.sleep(2) # long running job
 finally:
     rt.stop()
     
