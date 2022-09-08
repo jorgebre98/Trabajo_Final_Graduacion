6602dcb04aa3e8e62d81a28a05bd7c90d9f08d7d
Jorge Brenes <jorgebre2098@hotmail.com>
2022-09-08 10:02:58 -0600

    se agrega entrada manual

--- Data_Classes.py
+++ Data_Classes.py
@@ -55,6 +55,7 @@ class Inputs:
     def __init__(self,amplitude, time):
         self.A = amplitude
         self.t = time
+        self.contador = 0
 
 # Step Function
     def step_function(self):
@@ -65,6 +66,15 @@ class Inputs:
     def ramp_function(self):
         r = self.t*self.step_function()
         return r
+
+    def manual(self):
+        if keyboard.read_key() == 'a' or keyboard.read_key() == 'A':
+            self.contador += 5
+            print('Valor del contador: ', contador, flush = True)
+        elif keyboard.read_key() == 'd' or keyboard.read_key() == 'D':
+            self.contador -= 5
+            print('Valor del contador: ', contador, flush = True)
+
 
 # ********************************** Timer Class **********************************#
 class RepeatedTimer(object):
