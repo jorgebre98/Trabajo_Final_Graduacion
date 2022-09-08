6992b04f4d73a6fd9ae4bafb3f228c106e35dd43
Jorge Brenes <jorgebre2098@hotmail.com>
2022-09-07 23:28:34 -0600

    Se optimiza con clases para evitar errores de globales

--- Data_collection.py
+++ Data_collection.py
@@ -1,95 +1,40 @@
-## Tecnológico de Costa Rica
-### Autor: Jorge Andrés Brenes Alfaro
-## Data_collection.py
+from Data_Classes import *
 
-# The code presented allows an NVIDIA Jetson TX2 communicate with a PSoC 
-# with the purpose of collect data from a Péndulo amortiguado a hélice (PAHM) plant.
+# Definición del puerto serial
+serial_port  = serial.Serial("/dev/ttyTH2", baudrate = 115200,
+                             stopbits = serial.STOPBITS_ONE,
+                             bytesize = serial.EIGHTBITS,
+                             parity = serial.PARITY_NONE)
 
-#Libraries
-import time
-import serial
-import struct
-import random
-import xlsxwriter
-import numpy as np
-from Timer import *
-
-# ********************************** Step Function **********************************#
-def step_function(A,t):
-    u = (t >= 0)*A
-    return u
-
-# ********************************** Ramp Function **********************************#
-def ramp_function(A,t):
-    r = t*step_function(A,t)
-    return r
-
-# *********************** Function to create .xlsx archives *************************#
-def archivo_excel(values):
-    archive = xlsxwriter.Workbook("Data_collection.xlsx")
-    hoja = archive.add_worksheet()
-    names = ['Latencia','Entrada','Ángulo']
-    for j in range(len(names)):
-        hoja.write(0,j,names[j])
-
-    for j in range(len(values)):
-        for i in range(len(values[j])):
-            hoja.write(j+1,i,values[j][i]) # row, col, data
-    archive.close()
-
-# ********************************** Data Transmission and Reception **********************************#
-def Transmit_Receive(port,num):
-	global data_r, latencia
-	if port.inWaiting() > 0:
-		packed = struct.pack('!i',num)
-		ini = time.time()
-		port.write(packed)
-		data = port.read(size=4)
-		latencia = time.time()-ini
-		data_r = struct.unpack('!i',data)
-	return data_r[0],latencia
-
-def Data_collect(ser):
-    pwm_value = round(random.uniform(0,4),4)
-    pwm_value = 125+int(pwm_value/4*125)
-    data_receive, latencia = Transmit_Receive(ser, pwm_value)
-    print('\nData_transmit: ', pwm_value,'\nData receive: ',data_receive,flush=True)
-    return  [latencia,pwm_value,data_receive]
-    
-
-# ****** ***** Definicion del puerto ****** *******
-serial_port = serial.Serial("/dev/ttyTHS2",
-                            baudrate=115200,
-                            stopbits=serial.STOPBITS_ONE,
-                            bytesize=serial.EIGHTBITS,
-                            parity=serial.PARITY_NONE)
-time.sleep(1)
+time.sleep(0.5) #Dar tiempo para asignación de pines
 
 
-#star, end, sampling = -2,10,0.02
-#tiempo = np.concatenate([np.arange(star,end,sampling), np.zeros(1000)])
-#Amplitude = round(random.uniform(0,4),4)
-#step = step_function(Amplitude,tiempo)
-#r = ramp_function(Amplitude,tiempo)
-#print('\r\nAmplitud: ',Amplitude,'\r\n', flush = True)
+uart = TransmitReceive(serial_port)
 
 print('************ Starting *************', flush=True)
-serial_port.reset_input_buffer()
-serial_port.reset_output_buffer()
+uart.reset()
+#uart.create_pwm()
 
-rt = RepeatedTimer(0.02, Data_collect, serial_port) # No need of rt.start()
+rt = RepeatedTimer(0.02, uart.Transmit_Receive) # No need of rt.start()
 try:
     time.sleep(20) # long running job
 
 except KeyboardInterrupt:
     print("Exiting Program")
+    uart.reset()
+    serial_port.close()
+    #uart.turn_off()
 
 except Exception as exception_error:
     print("Error occurred.")
     print("Error: " + str(exception_error))
+    uart.reset()
+    serial_port.close()
+    #uart.turn_off()
 
 finally:
     rt.stop()
-    archivo_excel(rt.values)
+    uart.csv.doc()
+    uart.reset()
     serial_port.close()
     pass
