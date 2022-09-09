d66f96a282953076919d46bec309ccde2a1fd8d5
JorgeBrenes <jorgebre2098@hotmail.com>
2022-09-09 17:35:00 -0600

    Se modifica para hacer eco

--- programa.py
+++ programa.py
@@ -9,15 +9,17 @@ serial_port = serial.Serial("/dev/ttyTHS2",baudrate=115200,stopbits=serial.STOPB
 # PRUEBA 1
 serial_port.reset_input_buffer()
 serial_port.reset_output_buffer()
+contador = 0
 try:
         while True:
-                contador = 280#125+(random.randint(0,4)/4)*125 #int(input('PWM: '))
+                #contador = 0#125+(random.randint(0,4)/4)*125 #int(input('PWM: '))
                 if serial_port.inWaiting() > 0:
                         packed = struct.pack('!i',contador)
                         serial_port.write(packed)
                         receive = serial_port.read(4)
                         data = struct.unpack('!i',receive)
                         print('Dato Transmitido: {0} y Dato recibido: {1}'.format(contador,data[0]))
+                contador += 1
 
 except KeyboardInterrupt:
         print("Exiting Program")
