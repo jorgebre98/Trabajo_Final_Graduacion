2e717dfb1514f5114274ecf15baeb5615e4e8bd0
JorgeBrenes <jorgebre2098@hotmail.com>
2022-09-16 16:48:27 -0600

    Se corrige errores en el manual y playback con tamaños

--- Data_Collection/Transmit_and_Receive.py
+++ Data_Collection/Transmit_and_Receive.py
@@ -26,22 +26,21 @@ class TransmitReceive:
         self.lantency = 0
         self.contador = 0
         self.cont = 0
-        self.values = np.array([[0,0,0]])#[[0,0,0]]
+        self.values = np.array([[0,0,0]])
 
     def denormalizePWM(self,normalizedValue):
-        return int(normalizeValue*1000+1000)
+        return int(normalizedValue*1000+1000)
 
     def Transmit_Receive (self):
         #       Send 2 bytes
         if np.isscalar(self.pwm):
-            pwmval=self.pwm
-        else:          
-            if self.contador>=len(self.pwm):
-                self.contador=0
-            else:
-                self.contador+=1
-                  
-            pwmval=self.pwm[self.contador]
+            pwmval = self.pwm
+        else:
+            pwmval = self.pwm[self.contador]
+            
+            self.contador += 1          
+            if self.contador >= len(self.pwm):
+                self.contador = 0
                 
         packed = pack('!h',self.denormalizePWM(pwmval))
                 
@@ -55,8 +54,7 @@ class TransmitReceive:
             self.angle = unpack('!i',data)
 
             #       Save latency, transmit and receive data
-            val = np.array([[self.latency, pwmval, self.angle[0]*0.4]])
-            self.values = np.append(self.values,val, axis=0)
+            self.values = np.append(self.values,np.array([[self.latency, pwmval, self.angle[0]*0.4]]), axis=0)
    
     def reset(self):
         #   Clean transmit and receive buffers.
@@ -66,16 +64,10 @@ class TransmitReceive:
     def turn_off(self):
         self.pwm = 0
         self.port.write(pack('!i',self.pwm))
-
-    def pwm_ramp_step(self):
-        if self.contador <= 1220:
-            self.contador += 1
-        self.pwm = (self.contador-1000)/1000.0
     
     def pwm_set_safe_value(self, value):
-        self.pwm=np.min(value,0.25)
+        self.pwm = np.minimum(value,0.25)
         
-    
     def csv_doc(self, filename):
         with open(filename, 'w', newline='') as file:
             doc = csv.writer(file, delimiter=',')
