df74e3f5fc17e9330266d6a50366cd5152c4bc1d
JorgeBrenes <jorgebre2098@hotmail.com>
2022-09-20 17:41:27 -0600

    se hace un script para hacer varias corridas

---
+++ Data_Collection/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+python3 -O Data_collection.py --input 'Manual_1.csv' --output 'Data_1.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_2.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_3.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_4.csv'
+python3 -O Data_collection.py --input 'Data_3.csv' --output 'Data_5.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_6.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_7.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_8.csv'
+python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_9.csv'
+python3 -O Data_collection.py --input 'Data_8.csv' --output 'Data_10.csv'
