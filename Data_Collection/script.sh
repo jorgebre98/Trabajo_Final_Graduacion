c780f5eb122eef18ba976362027d5784e8d5b309
JorgeBrenes <jorgebre2098@hotmail.com>
2022-09-22 17:40:29 -0600

    Se cambia prueba

--- Data_Collection/script.sh
+++ Data_Collection/script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-python3 -O Data_collection.py --input 'Manual_1.csv' --output 'Data_1.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_2.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_3.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_4.csv'
-python3 -O Data_collection.py --input 'Data_3.csv' --output 'Data_5.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_6.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_7.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_8.csv'
-python3 -O Data_collection.py --input 'Data_1.csv' --output 'Data_9.csv'
-python3 -O Data_collection.py --input 'Data_8.csv' --output 'Data_10.csv'
+python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_1.csv'
+
+python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_2.csv'
+
+python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_3.csv'
+
+python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_4.csv'
+
