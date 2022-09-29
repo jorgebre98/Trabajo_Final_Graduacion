c6e365b72ca2e63def2f33607066d99cbc41aaa3
Jorge Brenes <jorgebre2098@hotmail.com>
2022-09-29 15:49:51 -0600

    Se agrega el weight and biases

--- Mimetizadora.py
+++ Mimetizadora.py
@@ -25,9 +25,22 @@ from tensorflow.keras.layers import Dense, Dropout, GRU, TimeDistributed
 import warnings
 warnings.filterwarnings('ignore')
 
+import wandb
+from wandb.keras import WandbCallback
+
+wandb login('52c82ce2e50072d968a0a256c901929128147287')
+wandb.init(project="Prueba", 
+           entity="mimetic-rna", 
+           name='Probando W&B',
+           resume='Allow', 
+           id='Probando W&B')
+wandb.config = {
+    "epochs": 500,
+    "batch_size": 8,
+    "learning_rate":0.001
+}
 
 #   ******************* Process the Dataset *******************
-
 Dir = os.listdir('/Users/jorge/Documents/TEC/TFG/Datos_Recolectados')
 pwm = np.array([])
 angle = np.array([])
@@ -93,14 +106,16 @@ model.add(Dropout(0.35))
 model.add(GRU(64, input_shape=(X_train.shape[1],1),return_sequences=True))
 model.add(Dropout(0.35))
 model.add(TimeDistributed(Dense(1))) # There is no difference between this and model.add(Dense(1))...
-model.compile(optimizer=Adam(), loss='mean_squared_error', metrics=['mse','acc'])
+model.compile(optimizer=Adam(learning_rate=0.001), loss='mean_squared_error', metrics=['mse','acc'])
 model.summary()
 
 #   Model Training
 history = model.fit(train_data, train_label,
                     epochs=500, batch_size=8,
                     validation_data = (val_data,val_label),
-                    verbose=2)
+                    verbose=2,
+                    callbacks=[WandbCallback()]
+                    )
 #   Prediction
 testPredict = model.predict(test_data)
 
