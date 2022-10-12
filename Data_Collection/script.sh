a4c52d59aca50cbd5ce7fd65fe6ac13b8afc262e
JorgeBrenes <jorgebre2098@hotmail.com>
2022-10-12 14:37:13 -0600

    Se modifica script para correr playback de valores altos

--- Data_Collection/script.sh
+++ Data_Collection/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_1.csv'
+python3 -O Data_collection.py --input 'Valores_altos.csv' --output 'Altos_3.csv'
 
-python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_2.csv'
+python3 -O Data_collection.py --input 'Altos_1.csv' --output 'Altos_4.csv'
 
-python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_3.csv'
+python3 -O Data_collection.py --input 'Altos_3.csv' --output 'Altos_5.csv'
 
-python3 -O Data_collection.py --input 'Entrada_modelo.csv' --output 'Respuesta_Escalon_4.csv'
+python3 -O Data_collection.py --input 'Valores_altos.csv' --output 'Altos_6.csv'
 
